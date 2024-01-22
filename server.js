0c219d1fc1297eb9d4bab5bfe12d5ab7fd6effa7
rohan-chari <rohan.rchari@gmail.com>
2024-01-22 11:07:37 -0500

    Set up homepage and routing

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
