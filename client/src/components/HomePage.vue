14c1ec10a0254a82a94ca4c7732f64d9bc0875e0
rohan-chari <rohan.rchari@gmail.com>
2024-01-23 16:49:17 -0500

    HomePage tweaks, env variables for heroku, and setting up userauth

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -2,33 +2,50 @@
     <div>
         <img src="../images/trivAI-logo.png">
         <v-card
-            width="600"
+            width="840"
             class="login-card"
-            title="Welcome to TrivAI"
-            subtitle="The best new way to take your trivia knowledge to the next level!"
-            text="This is content"
             color="white">
-                <v-row class="d-flex align-center justify-center">
-                    <v-col cols="auto"> <v-btn color="proceed"> Create Account / Login </v-btn> </v-col>
-                </v-row>
-                <v-row class="d-flex align-center justify-center">
-                    <v-col cols="auto">
-                        <v-btn color="info"> More Information </v-btn>
-                    </v-col>
-                </v-row>
+            <p class="text-h3">Put your trivia knowledge to the</p>
+            <p class="text-h3">test and level up with TrivAI.</p>
+            <p class="text-subtitle-1">Challenge your friends or play against AI opponents in trivia battles that will push your knowledge to new heights. With TrivAI, the next level of trivia competition is just a tap away.</p>
+            <v-row class="action-buttons d-flex align-center justify-center">
+                <v-col cols="auto"> <v-btn color="proceed"> Create Account / Login </v-btn> </v-col>
+                <v-col cols="auto">
+                    <v-btn color="info"> More Information </v-btn>
+                </v-col>
+            </v-row>
         </v-card>
+        
     </div>
 </template>
 <script>
 </script>
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+.text-h3{
+    font-family: "Rubik",sans-serif;
+    font-weight: 600;
+    margin-bottom: 20px;
+}
 h1{
     color: rgb(255, 255, 255);
     text-shadow: 2px 2px 2px black;
 }
 .login-card{
+    border-radius: 20px;
     margin-left: auto;
     margin-right: auto;
     padding-bottom: 40px;
+    padding-top: 40px;
+    padding-left: 30px;
+    padding-right:30px
+}
+.action-buttons{
+    margin-top:10px;
+}
+
+img{
+    width:300px;
+    height: 150px;
 }
 </style>
--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 const app = express();
+
+const { auth } = require('express-openid-connect');
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.auth0_secret,
+  baseURL: process.env.auth0_baseURL,
+  clientID: process.env.auth0_clientId,
+  issuerBaseURL: process.env.auth0_issuerBaseURL
+};
+
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/hmm', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  console.log(req.oidc.user)
+});
 
 // Serve static files from the Vue frontend app
 app.use(express.static(path.join(__dirname, 'client/dist')));
