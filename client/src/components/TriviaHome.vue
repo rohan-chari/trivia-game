636196ebd366761d9703abd78f0c4c6f15f233be
rohan-chari <rohan.rchari@gmail.com>
2024-01-25 10:09:00 -0500

    Set up trivia endpoint, generating questions and posting them to mongoDB

--- client/src/components/TriviaHome.vue
+++ client/src/components/TriviaHome.vue
@@ -1,46 +1,52 @@
 <template>
-    <div>
-        <h1>TriviaHome.vue</h1>
-        <v-dialog
-        transition="dialog-top-transition"
-        width="auto">
-            <template v-slot:activator="{ props }">
-            <v-btn
-                color="proceed"
-                v-bind="props"
-            >Quick Play</v-btn>
-            </template>
-            <template v-slot:default="{ isActive }">
-            <v-card>
-                <v-toolbar
-                color="primary"
-                title="Quick Play Settings"
-                ></v-toolbar>
+<div>
+    <h1>TriviaHome.vue</h1>
+    <v-dialog transition="dialog-top-transition" width="auto">
+        <template v-slot:activator="{ props }">
+            <v-btn color="proceed" v-bind="props">Quick Play</v-btn>
+        </template>
+        <template v-slot:default="{ isActive }">
+            <v-card width="500">
+                <v-toolbar color="primary" title="Quick Play Settings"></v-toolbar>
                 <v-card-text>
-                <div class="text-h2 pa-12">Hello world!</div>
+                    <v-select label="Choose your difficulty" :items="['Elementary School', 'High School', 'College', 'Genius']" v-model="selectedDifficulty">
+                    </v-select>
                 </v-card-text>
                 <v-card-actions class="justify-end">
-                <v-btn
-                    variant="text"
-                    @click="isActive.value = false"
-                >Close</v-btn>
+                    <v-btn variant="text" @click="isActive.value = false, selectedDifficulty = ''">Cancel</v-btn>
+                    <v-btn variant="text" :disabled="!selectedDifficulty" @click="startGame">Start Game</v-btn>
                 </v-card-actions>
             </v-card>
-            </template>
-        </v-dialog>
-    </div>
+        </template>
+    </v-dialog>
+</div>
 </template>
 
 <script>
-import { onMounted } from 'vue';
-//import axios from 'axios';
-
+import { ref } from 'vue';
+import axios from 'axios'
 export default {
     name: 'TriviaHome',
     setup() {
-        onMounted(() => {
+        const selectedDifficulty = ref('');
 
-        });
+        const startGame = async () => {
+            if (!selectedDifficulty.value) return;
+
+            try {
+                // Post the difficulty level to your backend
+                await axios.post('/api/quickplay/start', { difficulty: selectedDifficulty.value });
+                // Redirect handled by backend, or manually redirect here if needed
+                // window.location.href = '/QuickPlay';
+            } catch (error) {
+                console.error('Error starting game:', error);
+            }
+        };
+
+        return {
+            selectedDifficulty,
+            startGame
+        };
     }
 };
 </script>
--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const OpenAI = require('openai');
 const express = require('express');
 const path = require('path');
+var bodyParser = require('body-parser')
 require('dotenv').config();
 const app = express();
 const { auth } = require('express-openid-connect');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const uri = "mongodb+srv://rchari1:"+ process.env.mongoDBSecret +"@trivia-game-db.h56cz4h.mongodb.net/?retryWrites=true&w=majority";
 
-//db client setup
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+//Mongoose Setup
+mongoose.connect(uri)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+const Schema = mongoose.Schema;
+const triviaQuestionSchema = new Schema({
+  question: String,
+  choices: [String],
+  answer: String,
+  subject: String,
+  difficulty: String
 });
+const TriviaQuestion = mongoose.model('TriviaQuestion', triviaQuestionSchema);
+
+//bodyparser to read post requests
+app.use( bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 //openai configuration
 const openai = new OpenAI({
@@ -72,18 +84,45 @@ app.get('/api/auth-status', (req, res) => {
   res.json({ isAuthenticated: req.oidc.isAuthenticated() });
 });
 
-//GPT Question Generator Route
-app.post('/generate-questions', async (req, res) => {
+//QUICKPLAY Question Generator Route
+app.post('/api/quickplay/start', async (req, res) => {
   try {
+    let difficulty = req.body.difficulty
+    let prompt = generatePrompt(difficulty);
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "Can you generate me a trivia question from the NBA from 2007 with 4 answer choices? Can you make this question as obscure as possible? I want it to be an extremely difficult, random question that a hardcore basketball fan would be stumped. In addition, i need to pass these questions and answers to my front end application for a trivia game. Can you format these questions and answers in the following format: It will be an array, the first index will be a string, the second index will be an object with all 4 answer choices, and the third index will be the correct answer." }],
-      model: "gpt-3.5-turbo",
+      messages: [{ role: "system", content: prompt }],
+      model: "gpt-3.5-turbo-1106",
     });
-  
-    res.json(completion.choices[0]);
+    
+    let jsonData = JSON.parse(completion.choices[0].message.content);
+
+    let questionObject = {
+      question: jsonData.question , // Extract from completion
+      choices: jsonData.choices, // Extract and format from completion
+      answer: jsonData.answer, // Extract from completion
+      subject: jsonData.subject, // Extract or define based on difficulty
+      difficulty: difficulty
+    };
+
+    const existingQuestion = await TriviaQuestion.findOne({ question: questionObject.question, difficulty:questionObject.difficulty });
+    
+    if (existingQuestion) {
+      res.json({ message: "Question already exists" });
+    } else {
+      // Create a new TriviaQuestion and save it
+      const newQuestion = new TriviaQuestion(questionObject);
+      await newQuestion.save();
+      console.log("Message saved successfully")
+      res.redirect(`/QuickPlay/${questionObject.difficulty}`)
+    }
+
+    console.log('Question Object',questionObject);
+
+    //res.json(completion.choices[0]);
 
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    console.log(error)
+    res.status(500).json({ error: "error" });
   }
 });
 
@@ -94,15 +133,26 @@ app.listen(port, () => {
 });
 
 
-/*
-connect to mongodb
-try {
-  // Connect the client to the server	(optional starting in v4.7)
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
-} finally {
-  // Ensures that the client will close when you finish/error
-  await client.close();
-}*/
+const generatePrompt = (difficulty) => {
+  let topicDescription; // Customize this based on the difficulty
+  switch (difficulty) {
+    case 'Elementary School':
+      topicDescription = "Generate a trivia question that is simple, clear, and suitable for an elementary school student. The question should challenge children in this age group without being too complex for them, yet it should be too straightforward for high school students or adults.";
+      break;
+    case 'High School':
+      topicDescription = 'Generate a trivia question that is engaging and challenging for a high school student. The question should be complex enough to test the knowledge typical of this educational level but not too advanced for someone not in college.';
+      break;
+    case 'College':
+      topicDescription = 'Generate a trivia question that is suitable for a college student, requiring a deeper level of understanding and critical thinking. The question should be challenging for individuals who have not received higher education.';
+      break;
+    case 'Genius':
+      topicDescription = 'Generate a trivia question that is highly challenging and suitable for an expert in a specific field. The question should require specialized knowledge and be difficult for those without professional or advanced expertise in the subject area.';
+      break;
+    default:
+      topicDescription = 'Generate a general knowledge trivia question that is interesting and accessible to a broad audience. The question should not require specialized knowledge and should be suitable for various ages and educational backgrounds.';
+  }
+  return `Generate a unique trivia question focusing on the following themes: superheroes. Each question should explore specific details, revealing fascinating and lesser-known information within these themes. Ensure the questions are captivating, thought-provoking, and uncommon in standard trivia collections. Aim to challenge trivia enthusiasts with novel and surprising topics, avoiding repetition and well-known trivia subjects. Creatively expand on these themes to offer a rich variety of questions. ${topicDescription}.  Format the response as a JSON object, strictly adhering to the following structure: {"question": "<question_text>", "choices": ["<choice1>", "<choice2>", "<choice3>", "<choice4>"], "answer": "<correct_answer>", "subject": "<selected_topic>", "difficulty": "${difficulty}"}. Please do not include any additional text or information outside of this JSON structure.`
+  //return `Generate a unique trivia question focusing on one of the following themes: obscure historical events, lesser-known scientific discoveries, unusual cultural practices around the world, niche aspects of sports, hidden gems in the entertainment industry, or intriguing geography facts. Please do a good job of choosing a random one of these topics as it is of utmost importance that if I want more questions, I get a an equal chance of getting a different subject. Each question should explore specific details, revealing fascinating and lesser-known information within these themes. Ensure the questions are captivating, thought-provoking, and uncommon in standard trivia collections. Aim to challenge trivia enthusiasts with novel and surprising topics, avoiding repetition and well-known trivia subjects. Creatively expand on these themes to offer a rich variety of questions. ${topicDescription}.  Format the response as a JSON object, strictly adhering to the following structure: {"question": "<question_text>", "choices": ["<choice1>", "<choice2>", "<choice3>", "<choice4>"], "answer": "<correct_answer>", "subject": "<selected_topic>", "difficulty": "${difficulty}"}. Please do not include any additional text or information outside of this JSON structure.`
+}
+
+
