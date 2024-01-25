01500ce94f66ad08fae309a8022a158b2950e086
rohan-chari <rohan.rchari@gmail.com>
2024-01-25 15:38:35 -0500

    added mobile responsiveness in quickplay and colored the buttons

--- client/src/components/QuickPlay.vue
+++ client/src/components/QuickPlay.vue
@@ -1,24 +1,29 @@
 <template>
-    <div>
-        <h1>QuickPlay.vue</h1>
-        <div v-if="allTriviaQuestions && allTriviaQuestions.length > 0">
-            <div class="currentQuestion">
-                <h2>{{ currentQuestion.question }}</h2>
-                <div v-for="(answer, index) in currentQuestion.choices" :key="index">
-                    <button @click="selectAnswer(answer)">{{ answer }}</button>
-                </div>
-            </div>
-        </div>
-        <div v-else>
-            Loading questions...
-        </div>
+<div>
+    <h1>QuickPlay.vue</h1>
+    <div v-if="allTriviaQuestions && allTriviaQuestions.length > 0">
+        <p class="text-h3">{{ currentQuestion.question }}</p>
+        <v-row justify="center">
+            <v-col cols="12" lg="3" md="3" xl="3" sm="12" v-for="(answer, index) in currentQuestion.choices.slice(0, 2)" :key="index">
+                <v-btn color="proceed" block @click="selectAnswer(answer)">{{ answer }}</v-btn>
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="12" lg="3" md="3" xl="3" sm="12" v-for="(answer, index) in currentQuestion.choices.slice(2, 4)" :key="index + 2">
+                <v-btn color="proceed" block @click="selectAnswer(answer)">{{ answer }}</v-btn>
+            </v-col>
+        </v-row>
     </div>
+    <div v-else>
+        Loading questions...
+    </div>
+</div>
 </template>
-    
+
 <script>
-import { onMounted, ref } from 'vue';
+import {onMounted,ref} from 'vue';
 import axios from 'axios';
-import { useRoute } from 'vue-router';
+import {useRoute} from 'vue-router';
 
 export default {
     name: 'QuickPlay',
@@ -33,7 +38,7 @@ export default {
         const fetchTriviaQuestion = async () => {
             try {
                 const response = await axios.get('/api/quickplay/start', {
-                    params:{
+                    params: {
                         difficulty: difficulty.value
                     }
                 });
@@ -46,7 +51,7 @@ export default {
 
         const selectAnswer = (answer) => {
             // Logic to check if the answer is correct
-            if(answer == currentQuestion.value.answer){
+            if (answer == currentQuestion.value.answer) {
                 console.log('you got the answer correct!')
             }
             // Move to next question
@@ -72,3 +77,15 @@ export default {
 };
 </script>
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+.text-h3{
+    font-family: "Rubik",sans-serif;
+    font-weight: 600;
+    margin-bottom: 20px;
+}
+.answer-choices{
+    margin-left:auto;
+    margin-right: auto;
+}
+</style>
